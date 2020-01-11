7325553817a632e66f4b4550baede45f38021d98
Jerry Kizilos <jerrykiz@hotmail.com>
2020-01-11 11:51:09 +0100

    renaming file

--- create-stack.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws cloudformation create-stack --stack-name servers --template-body file://launch_config.yml --parameters file://web-app.json --disable-rollback --capabilities CAPABILITY_IAM
