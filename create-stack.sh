dc934708dec63589aea517814d133fb0e8c56bc5
Jerry Kizilos <jerrykiz@hotmail.com>
2020-01-11 11:47:20 +0100

    created create-stack.sh

---
+++ create-stack.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aws cloudformation aws cloudformation create-stack --stack-name servers --template-body file://launch_config.yml --parameters file://web-app.json --disable-rollback --capabilities CAPABILITY_IAM
