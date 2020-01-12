8db7fc832a180eddd9c7efc549a3481cf52709df
Jerry Kizilos <jerrykiz@hotmail.com>
2020-01-12 18:27:35 +0100

    update: create-stack-servers.sh takes servers.yml as argument now

--- create-stack-servers.sh
+++ create-stack-servers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation create-stack --stack-name servers --template-body file://launch_config.yml --parameters file://parameters-servers.json --disable-rollback --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name servers --template-body file://servers.yml --parameters file://parameters-servers.json --disable-rollback --capabilities CAPABILITY_IAM
